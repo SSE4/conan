3869cdbf72a957d890d7ac9e8733a891a70ea73e
SSE4 <tomskside@gmail.com>
2019-06-24 23:43:52 +0200

    - enable logging for requests
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conans/util/log.py
+++ conans/util/log.py
@@ -3,6 +3,20 @@ import sys
 from logging import StreamHandler
 
 
+import requests
+import logging
+
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+
 class MultiLineFormatter(logging.Formatter):
     def format(self, record):
         str_ = logging.Formatter.format(self, record)
@@ -20,6 +34,9 @@ class MultiLineFormatter(logging.Formatter):
 
 def configure_logger(logging_level=logging.CRITICAL, logging_file=None):
     # #### LOGGER, MOVED FROM CONF BECAUSE OF MULTIPLE PROBLEM WITH CIRCULAR INCLUDES #####
+
+
+
     logger = logging.getLogger('conans')
     if logging_file is not None:
         hdlr = logging.FileHandler(logging_file)
@@ -33,6 +50,11 @@ def configure_logger(logging_level=logging.CRITICAL, logging_file=None):
         logger.removeHandler(hand)
     logger.addHandler(hdlr)
     logger.setLevel(logging_level)
+
+    # You must initialize logging, otherwise you'll not see debug output.
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
     return logger
 
 
